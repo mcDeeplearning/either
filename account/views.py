644e74cdaa6d6aa1a45caf645bb9335c6ba02d0e
changhee <change@hphk.kr>
2018-11-15 04:32:18 +0000

    profile

--- account/views.py
+++ account/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from account.forms import ProfileForm
 
 # Create your views here.
 def login(request):
+    # 이미 로그인 했으면서 또 로그인 페이지 보여달라고 할때
+    if request.user.is_authenticated:
+        return redirect('question:list')
+        
     #사용자가 로그인을 요청했으면?
     if request.method == "POST": 
         form =  AuthenticationForm(request, request.POST)
@@ -25,12 +30,17 @@ def signup(request):
     if request.method=="POST":
         # 사용자를 등록하는 로직
         form = UserCreationForm(request.POST)
-        if form.is_valid():
+        pro_form = ProfileForm(request.POST)
+        if form.is_valid() and pro_form.is_valid():
             user = form.save()
+            profile = pro_form.save(commit=False)
+            profile.user = user
+            profile.save()
             auth_login(request, user)
             return redirect('question:list')
         
     else: 
         # 정보를 입력하는 폼을 전달
         form = UserCreationForm()
-    return render(request,'account/signup.html',{'form':form})
+        pro_form = ProfileForm()
+    return render(request,'account/signup.html',{'form':form,'pro_form':pro_form})
